62d04fecb950af39cbee7ff765ea4e6025c7b377
Doug C. Hardester <doug@swankibs.com>
2019-07-16 18:32:18 -0400

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# vscode-mdi-svg
-VS Code SVG Snippets for Material Design Icons
+# Material Design Icon SVG Snippets for VS Code
+
+Easily insert Material Design Icons into your code with these SVG snippets. Just start by typing `svg-mdi-[name]`.
+
+[http://r3v.in/5Z30K/Screen-Recording-2019-07-16-18-24-06.gif](Animation showing snippet functionality.)
+
+## Features
+
+These snippets are set up to work with `html`, `javascript`, `react`, `vue`, `xml` and `razor`.
+
+
+### 1.0.0
+
+Initial release
---
+++ build.js
@@ -0,0 +1,23 @@
+const util = require('@mdi/util');
+
+const fileName = "./snippets/snippets.json";
+const version = util.getVersion();
+const meta = util.getMeta(true); // withPaths
+
+const body = meta.map(icon => 
+  `"mdi-${icon.name}" : {
+    "body": "<svg viewBox=\\"0 0 24 24\\"><path d=\\"${icon.path}\\" /></svg>",
+    "description": "mdi-${icon.name}",
+    "prefix": "svg-mdi-${icon.name}"
+  },
+  `).join('');
+
+const template = 
+  `{
+    ${body}
+  }
+  `;
+
+util.write(fileName, template);
+
+console.log(`\u2714 Build ${version}`);
